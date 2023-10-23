300e29155551c7b73b19c0abcd344dfdfff908bb
Preedee Ponchevin <nickmsft@mac.com>
2023-10-23 16:47:50 +0700

    Initial Project

---
+++ src/apis.rs
@@ -0,0 +1,12 @@
+use actix_web::{HttpResponseBuilder, Responder, web};
+use actix_web::http::StatusCode;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug,Clone,Serialize,Deserialize)]
+pub struct PiiLogRequest {
+
+}
+pub async fn post_piilog_func(payload: web::Json<PiiLogRequest>) -> impl Responder {
+    HttpResponseBuilder::new(StatusCode::OK).finish()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use actix_web::{App, HttpServer, web};
+use crate::apis::post_piilog_func;
+
+mod apis;
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port_key = "FUNCTIONS_CUSTOMHANDLER_PORT";
+    let port: u16 = match env::var(port_key) {
+        Ok(val) => val.parse().expect("Custom Handler port is not a number!"),
+        Err(_) => 3000,
+    };
+
+    HttpServer::new(|| {
+        App::new().service(
+            // prefixes all resources and routes attached to it...
+            web::scope("/api")
+                // ...so this handles requests for `GET /app/index.html`
+                .route("/PiiLogHttpTrigger", web::post().to(post_piilog_func)),
+        )
+    })
+        .bind(("0.0.0.0", port))?
+        .run()
+        .await
 }
