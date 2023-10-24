dee7521456080bfda1f198267affc219e708b853
Preedee Ponchevin <nickmsft@mac.com>
2023-10-24 20:39:57 +0700

    - Add get environment variable - Add Configuration to rest api endpoint

--- src/apis.rs
+++ src/apis.rs
@@ -2,13 +2,12 @@ use std::fmt::Display;
 
 use actix_web::{HttpRequest, web};
 use azure_security_keyvault::prelude::KeyVaultGetSecretResponse;
+use kafka::client::{KafkaClient, SecurityConfig};
 use logs::debug;
 use openssl::ssl::{SslConnector, SslMethod, SslVerifyMode};
 use openssl::x509::X509;
 
-use crate::models::{
-    PiiLogFuncConfiguration, PiiLogFuncResult, PiiLogRequest, PiiLogResponse,
-};
+use crate::models::{PiiLogFuncConfiguration, PiiLogFuncResult, PiiLogRequest, PiiLogResponse};
 
 pub async fn post_piilog_func(
     req: HttpRequest,
@@ -25,52 +24,50 @@ pub async fn post_piilog_func(
 
     let idx_end_private_key = cert_value.clone().find(end_private_key);
     let private_key = match idx_end_private_key {
-        None => {
-            ""
-        }
+        None => "",
         Some(s) => {
             let end = s + end_private_key.len();
             match cert_value.get(0..end) {
-                None => {
-                    ""
-                }
-                Some(c) => {
-                    c
-                }
+                None => "",
+                Some(c) => c,
             }
         }
     };
     let certificate_key = match cert_value.clone().find(start_certificate_key) {
-        None => {
-            ""
-        }
+        None => "",
         Some(s) => {
             let start = s;
             match cert_value.get(start..) {
-                None => {
-                    ""
-                }
-                Some(c) => {
-                    c
-                }
+                None => "",
+                Some(c) => c,
             }
         }
     };
-
-    debug!("Private key \r\n{}",private_key);
-    debug!("Certificate key \r\n{}",certificate_key);
+    debug!("Private key \r\n{}", private_key);
+    debug!("Certificate key \r\n{}", certificate_key);
 
     let mut builder = SslConnector::builder(SslMethod::tls()).unwrap();
     builder.set_cipher_list("DEFAULT").unwrap();
     builder.set_verify(SslVerifyMode::PEER);
-
-//    let cert_bytes = general_purpose::STANDARD
-//        .decode(data_cert.value.as_str()).unwrap();
-//    debug!("{:?}", cert_bytes);
+    //    let cert_bytes = general_purpose::STANDARD
+    //        .decode(data_cert.value.as_str()).unwrap();
+    //    debug!("{:?}", cert_bytes);
 
     let x509 = X509::from_pem(certificate_key.as_bytes()).unwrap();
     builder.set_certificate(&x509).unwrap();
     let connector = builder.build();
+    // ~ instantiate KafkaClient with the previous OpenSSL setup
+    let kafka_brokers = data_config
+        .kafka_endpoint
+        .split(",")
+        .map(|c| c.to_string())
+        .collect::<Vec<String>>();
+
+    debug!("Kafka brokers connected {:?}",kafka_brokers);
+    let mut client = KafkaClient::new_secure(
+        kafka_brokers,
+        SecurityConfig::new(connector).with_hostname_verification(true),
+    );
 
 
     Ok(PiiLogResponse {
