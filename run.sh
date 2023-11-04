c5df065e86d01c13f378508dbf41d2c052796e72
Preedee Ponchevin <nickmsft@mac.com>
2023-11-05 00:27:22 +0700

    - Add kafka - Add Self sign cert + kafka tls

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
- export DOCKER_KAFKA_HOST=$(ipconfig getifaddr en0)
  PII_LOG_ENDPOINT="kafka.preedee.space:9092" \
  PII_LOG_KEY_VAULT_ACCOUNT="nicksecretstoredev001" \
- PII_LOG_KEY_VAULT_KEY_NAME="certkafkadevnick006" \
+ PII_LOG_KEY_VAULT_KEY_NAME="certkafkadevnick100" \
  RUST_LOG=piilog_func cargo run
--- src/apis.rs
+++ src/apis.rs
@@ -1,11 +1,18 @@
 use std::fmt::Display;
 use std::time::Duration;
 
-use actix_web::{HttpRequest, web};
+use actix_web::{web, HttpRequest};
+use azure_core::base64::encode;
 use azure_security_keyvault::prelude::KeyVaultGetSecretResponse;
+use base64::{DecodeError, Engine};
+use base64::engine::general_purpose;
 use kafka::client::{KafkaClient, ProduceMessage, RequiredAcks, SecurityConfig};
 use logs::{debug, error};
+use openssl::error::ErrorStack;
+use openssl::pkcs12::{ParsedPkcs12_2, Pkcs12};
+use openssl::pkey::PKeyRef;
 use openssl::ssl::{SslConnector, SslFiletype, SslMethod, SslVerifyMode};
+use openssl::x509::X509;
 
 use crate::models::{
     PiiLogFuncConfiguration, PiiLogFuncError, PiiLogFuncResult, PiiLogRequest, PiiLogResponse,
@@ -22,23 +29,49 @@ pub async fn post_pii_log_func(
     let mut builder = SslConnector::builder(SslMethod::tls()).unwrap();
     builder.set_cipher_list("DEFAULT").unwrap();
     builder.set_verify(SslVerifyMode::PEER);
+
+
+    let pfx_data = data_cert.value.clone();
+    match general_purpose::STANDARD.decode(pfx_data) {
+        Ok(c) => {
+            let pkcs12 = Pkcs12::from_der(&c).unwrap().parse2("");
+            match pkcs12 {
+                Ok(pk) => {
+                    if let (Some(pk) , Some(cert)) = (pk.pkey , pk.cert) {
+                        builder.set_private_key(pk.as_ref()).unwrap();
+                        builder.set_certificate(cert.as_ref()).unwrap();
+
+                        builder.check_private_key().expect("Private key failed");
+                    }
+                }
+                Err(e) => {
+                    error!("Error get pkcs12: {}", e);
+                }
+            }
+        }
+        Err(e) => {
+            error!("Error decoding certificate : {}", e);
+        }
+    }
+    /*
     builder
         .set_certificate_file(
-            "/Users/preedee/Project/Kafka/kafka_2.12-3.5.1/ssl/server.pem",
+            "/Users/preedee/Project/Kafka/kafka_2.12-3.5.1/ssl2/kafka.preedee.space.signed.crt.pem",
             SslFiletype::PEM,
         )
         .unwrap();
     builder
         .set_private_key_file(
-            "/Users/preedee/Project/Kafka/kafka_2.12-3.5.1/ssl/server.key.pem",
+            "/Users/preedee/Project/Kafka/kafka_2.12-3.5.1/ssl2/kafka.preedee.space.p12.key.pem",
             SslFiletype::PEM,
         )
         .unwrap();
-    builder.check_private_key().expect("Private key failed");
 
+            builder.check_private_key().expect("Private key failed");
+    */
 
     builder
-        .set_ca_file("/Users/preedee/Project/Kafka/kafka_2.12-3.5.1/ssl/ca.pem")
+        .set_ca_file("/Users/preedee/Project/Kafka/kafka_2.12-3.5.1/ssl2/rootCA.crt.pem")
         .unwrap();
 
     //builder.set_ca_file("/Users/preedee/Project/kafka/kafka-docker/certs/kafka.keystore.jks.crt").unwrap();
@@ -51,11 +84,12 @@ pub async fn post_pii_log_func(
         .filter(|c| !c.is_empty())
         .collect::<Vec<String>>();
 
-    debug!("List Kafka brokers : {:?}", kafka_brokers);
-    let mut client = KafkaClient::new_secure(
-        kafka_brokers,
-        SecurityConfig::new(connector).with_hostname_verification(false),
+    let config = SecurityConfig::new(connector).with_hostname_verification(false);
+    debug!(
+        "List Kafka brokers : {:?} with config : {:#?} ",
+        kafka_brokers, config,
     );
+    let mut client = KafkaClient::new_secure(kafka_brokers, config);
     match client.load_metadata_all() {
         Ok(_) => {
             let req = vec![
