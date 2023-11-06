bd5d18ab20cfa694a2896536ddb58434936576fc
Preedee Ponchevin <nickmsft@mac.com>
2023-11-06 19:54:40 +0700

    - Add kafka - Add Self sign cert + kafka tls

--- src/apis.rs
+++ src/apis.rs
@@ -1,18 +1,14 @@
 use std::fmt::Display;
 use std::time::Duration;
 
-use actix_web::{web, HttpRequest};
-use azure_core::base64::encode;
+use actix_web::{HttpRequest, web};
 use azure_security_keyvault::prelude::KeyVaultGetSecretResponse;
-use base64::{DecodeError, Engine};
+use base64::Engine;
 use base64::engine::general_purpose;
 use kafka::client::{KafkaClient, ProduceMessage, RequiredAcks, SecurityConfig};
 use logs::{debug, error};
-use openssl::error::ErrorStack;
-use openssl::pkcs12::{ParsedPkcs12_2, Pkcs12};
-use openssl::pkey::PKeyRef;
-use openssl::ssl::{SslConnector, SslFiletype, SslMethod, SslVerifyMode};
-use openssl::x509::X509;
+use openssl::pkcs12::Pkcs12;
+use openssl::ssl::{SslConnector, SslMethod, SslVerifyMode};
 
 use crate::models::{
     PiiLogFuncConfiguration, PiiLogFuncError, PiiLogFuncResult, PiiLogRequest, PiiLogResponse,
@@ -30,29 +26,32 @@ pub async fn post_pii_log_func(
     builder.set_cipher_list("DEFAULT").unwrap();
     builder.set_verify(SslVerifyMode::PEER);
 
-
     let pfx_data = data_cert.value.clone();
-    match general_purpose::STANDARD.decode(pfx_data) {
+    let res_decode = match general_purpose::STANDARD.decode(pfx_data) {
         Ok(c) => {
             let pkcs12 = Pkcs12::from_der(&c).unwrap().parse2("");
             match pkcs12 {
                 Ok(pk) => {
-                    if let (Some(pk) , Some(cert)) = (pk.pkey , pk.cert) {
+                    if let (Some(pk), Some(cert)) = (pk.pkey, pk.cert) {
+                        debug!("Set private key and certificate");
                         builder.set_private_key(pk.as_ref()).unwrap();
                         builder.set_certificate(cert.as_ref()).unwrap();
-
                         builder.check_private_key().expect("Private key failed");
                     }
+                    Ok(())
                 }
                 Err(e) => {
-                    error!("Error get pkcs12: {}", e);
+                    //error!("Error get pkcs12: {}", e);
+                    Err(PiiLogFuncError::new(e.to_string()))
                 }
             }
         }
         Err(e) => {
-            error!("Error decoding certificate : {}", e);
+            //error!("Error decoding certificate : {}", e);
+            Err(PiiLogFuncError::new(e.to_string()))
         }
-    }
+    };
+
     /*
     builder
         .set_certificate_file(
@@ -69,43 +68,60 @@ pub async fn post_pii_log_func(
 
             builder.check_private_key().expect("Private key failed");
     */
-
     builder
         .set_ca_file("/Users/preedee/Project/Kafka/kafka_2.12-3.5.1/ssl2/rootCA.crt.pem")
         .unwrap();
 
-    //builder.set_ca_file("/Users/preedee/Project/kafka/kafka-docker/certs/kafka.keystore.jks.crt").unwrap();
-    let connector = builder.build();
-    // ~ instantiate KafkaClient with the previous OpenSSL setup
-    let kafka_brokers = data_config
-        .kafka_endpoint
-        .split(",")
-        .map(|c| c.to_string())
-        .filter(|c| !c.is_empty())
-        .collect::<Vec<String>>();
-
-    let config = SecurityConfig::new(connector).with_hostname_verification(false);
-    debug!(
-        "List Kafka brokers : {:?} with config : {:#?} ",
-        kafka_brokers, config,
-    );
-    let mut client = KafkaClient::new_secure(kafka_brokers, config);
-    match client.load_metadata_all() {
+    match res_decode {
         Ok(_) => {
-            let req = vec![
-                ProduceMessage::new("piilog", 0, None, Some("a|b|a".as_bytes())),
-                ProduceMessage::new("piilog", 0, None, Some("b|b|b".as_bytes())),
-            ];
-            let resp = client.produce_messages(RequiredAcks::One, Duration::from_millis(100), req);
+            let connector = builder.build();
+            // ~ instantiate KafkaClient with the previous OpenSSL setup
+            let kafka_brokers = data_config
+                .kafka_endpoint
+                .split(",")
+                .map(|c| c.to_string())
+                .filter(|c| !c.is_empty())
+                .collect::<Vec<String>>();
 
-            debug!("Response from Kafka broker: {:#?}", resp);
-            Ok(PiiLogResponse {
-                message: "Sent Completed".to_string(),
-            })
+            let config = SecurityConfig::new(connector).with_hostname_verification(false);
+            debug!(
+                "List Kafka brokers : {:?} with config : {:#?} ",
+                kafka_brokers, config,
+            );
+            let mut client = KafkaClient::new_secure(kafka_brokers, config);
+            match client.load_metadata_all() {
+                Ok(_) => {
+
+                    for topic in client.topics().names() {
+                        debug!("topic: {}", topic);
+                    }
+
+                    let req = vec![ProduceMessage::new(
+                        "piiLog",
+                        0,
+                        None,
+                        Some("a|b|a".as_bytes()),
+                    )];
+                    let resp =
+                        client.produce_messages(RequiredAcks::One, Duration::from_millis(100), req);
+                    resp.map(|r| {
+                        debug!("Response received : {:#?}", r);
+                        PiiLogResponse {
+                            message: "Sent Completed".to_string(),
+                        }
+                    })
+                        .map_err(|e| {
+                            error!("Produce message error : {:?}", e);
+                            PiiLogFuncError::new(e.to_string())
+                            }
+                        )
+                }
+                Err(e) => {
+                    error!("Load Metadata failed : {:?}", e);
+                    Err(PiiLogFuncError::new(e.to_string()))
+                }
+            }
         }
-        Err(e) => {
-            error!("Load Metadata failed : {:?}", e);
-            Err(PiiLogFuncError::new(e.to_string()))
-        }
+        Err(e) => Err(e),
     }
 }
